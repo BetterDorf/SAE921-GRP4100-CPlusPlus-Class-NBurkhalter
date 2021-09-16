242cac92271caa3cb483117fb30829ea01cc952e
dorF <nemoz.burkhalter@gmail.com>
2021-09-16 11:36:51 +0200

    Ajout visual studio

---
+++ CPlusPlus-Class/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+
+
+int rollDice()
+{
+	return rand() % 6 + 1;
+}
+
+int rollMultiple(int x)
+{
+	int total = 0;
+	for (i)
+}
+
+int main()
+{
+    std::cout << rollDice() << std::endl;
+} 
