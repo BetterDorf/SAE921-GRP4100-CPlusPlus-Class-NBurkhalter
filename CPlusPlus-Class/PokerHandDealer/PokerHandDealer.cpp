5ff40de31161f986ef3257234ae7434de011d8d2
dorF <nemoz.burkhalter@gmail.com>
2021-09-22 19:27:48 +0200

    Implemented all of the hand value evaluator

--- CPlusPlus-Class/PokerHandDealer/PokerHandDealer.cpp
+++ CPlusPlus-Class/PokerHandDealer/PokerHandDealer.cpp
@@ -8,8 +8,9 @@ const int CARDS_TO_DRAW = 5;
 int const CARDS = 52;
 
 //Enum of values a card can have
-enum class Value
+enum class Value : int
 {
+    Ace,
     Two,
     Three,
 	Four,
@@ -22,7 +23,6 @@ enum class Value
     Jack,
     Queen,
     King,
-    Ace,
 
     nbValues
 };
@@ -100,11 +100,75 @@ bool inArray(int value, std::array<int, CARDS_TO_DRAW> cardsDrawn)
     return false;
 }
 
+//return 0 for no straight 1 for straight and 2 for royal
+int checkConsecutive(std::array<Value, CARDS_TO_DRAW> values)
+{
+    bool straight = true;
+    int biggestNum = 0;
+    //find biggest number once considering ace as the smallest
+	for (int i = 0 ; i < CARDS_TO_DRAW ; i++)
+	{
+        if (static_cast<int>(values.at(i)) > biggestNum)
+        {
+            biggestNum = static_cast<int>(values.at(i));
+        }
+	}
+
+    int loops = 0;
+    do
+    {
+        straight = true;
+        //search the number directly below it and repeat cards - 1 times
+        for (int consecutiveNums = 1; consecutiveNums < CARDS_TO_DRAW; consecutiveNums++)
+        {
+            bool found = false;
+            for (int i = 0; i < CARDS_TO_DRAW; i++)
+            {
+                if (biggestNum - static_cast<int>(values.at(i)) == 1)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                straight = false;
+                break;
+            }
+            biggestNum -= 1;
+        }
+        if (straight)
+        {
+            //if we are on our second loop then ace is the biggest and this has potential for a royal flush
+            if (loops == 1)
+                return 2;
+            return 1;
+        }
+
+        //if there's an ace in the loop, try again with 13 as biggest
+        if (loops == 0)
+        {
+            bool found = false;
+            for (int i = 0 ; i < CARDS_TO_DRAW ; i++ )
+            {
+	            if (values.at(i) == Value::Ace)
+	            {
+                    found = true;
+		            break;
+	            }
+            }
+            if (!found)
+                return 0;
+            biggestNum = static_cast<int>(Value::nbValues);
+        }
+    } while (loops++ < 1);
+
+    //by default we consider nothing was found
+    return 0;
+}
+
 void printHandValue(std::array<Value, CARDS_TO_DRAW> valuesDrawn, std::array<Suit, CARDS_TO_DRAW> suitsDrawn)
 {
-    //some reusable variables
-    int curCard = 0;
-
 	//register both straight and flushes
     bool flush = true;
     Suit suitOfHand = suitsDrawn.at(0);
@@ -118,26 +182,141 @@ void printHandValue(std::array<Value, CARDS_TO_DRAW> valuesDrawn, std::array<Sui
     }
 
     //check for straight
-    bool straight = true;
+    int straight = checkConsecutive(valuesDrawn);
 
-    //Check for straight flush
+    //Check for straight flush and Royal 
+    if (straight > 0 && flush)
+    {
+	    //Check for royal
+        if (straight == 2)
+        {
+	        //ROYAL
+            std::cout << "ROYAL FLUSH !!!!!!!" << std::endl;
+        }
+        else
+        {
+	        //Straight flush
+            std::cout << "Straight Flush !!!!!!" << std::endl;
+        }
+        return;
+    }
 
     //register the highest number of identical values
+    int mostRepeats = 0;
+    Value mostRepeatedValue;
+    //loop over each card
+    for (int i = 0 ; i < CARDS_TO_DRAW ; i++)
+    {
+        Value card = valuesDrawn.at(i);
+        int repeats = 0;
+
+        //check how many times that card occurs in the hand
+	    for (int j = 0 ; j < CARDS_TO_DRAW ; j++)
+	    {
+		    if (card == valuesDrawn.at(j))
+		    {
+                repeats++;
+		    }
+	    }
+        if (repeats > mostRepeats)
+        {
+            mostRepeats = repeats;
+            mostRepeatedValue = card;
+        }
+    }
+
     //Check if there is a four of a kind
+    if (mostRepeats == 4)
+    {
+        //Straight flush
+        std::cout << "Four of a kind !!!!!" << std::endl;
+    }
 
     //check if there is a three-of-a-kind and register the bool second pair (full house)
+    //register the second pair
+    bool secondPair = false;
+    for (int i = 0; i < CARDS_TO_DRAW; i++)
+    {
+        Value card = valuesDrawn.at(i);
+        int repeats = 0;
+
+        //make sure the card isn't the first pair/three/four of a kind
+        if (card == mostRepeatedValue)
+        {
+	        continue;
+        }
+
+        //check how many times that card occurs in the hand
+        for (int j = 0; j < CARDS_TO_DRAW; j++)
+        {
+            if (card == valuesDrawn.at(j))
+            {
+                repeats++;
+            }
+        }
+        if (repeats == 2)
+        {
+            secondPair = true;
+            break;
+        }
+    }
+
+    //full house
+    if (mostRepeats == 3 && secondPair)
+    {
+        std::cout << "Full House !!!!" << std::endl;
+        return;
+    }
 
     //check if a normal flush was achieved
+    if (flush)
+    {
+        std::cout << "Flush !!!!" << std::endl;
+        return;
+    }
 
     //check for straight
+    if (straight > 0)
+    {
+        std::cout << "Straight !!!" << std::endl;
+        return;
+    }
 
     //three of a kind
+    if (mostRepeats == 3)
+    {
+        std::cout << "Three of a kind !!!" << std::endl;
+        return;
+    }
 
     //a pair and another pair
+    if (mostRepeats == 2 && secondPair)
+    {
+        std::cout << "Two pairs !!" << std::endl;
+        return;
+    }
 
     //a pair
+    if (mostRepeats == 2)
+    {
+        std::cout << "Pair !" << std::endl;
+        return;
+    }
 
     //if none of those, print the highest value
+    int biggestNum = 0;
+    for (int i = 0; i < CARDS_TO_DRAW; i++)
+    {
+        if (valuesDrawn.at(i) == Value::Ace)
+        {
+            biggestNum = static_cast<int>(Value::nbValues);
+        }
+        if (static_cast<int>(valuesDrawn.at(i)) > biggestNum)
+        {
+            biggestNum = static_cast<int>(valuesDrawn.at(i));
+        }
+    }
+    std::cout << "Highest number : " << biggestNum + 1<< std::endl;
 }
 
 int main()
@@ -191,7 +370,7 @@ int main()
             cardsDrawn.at(card) = cardToDraw;
 
             //Add the corresponding values and suit
-            valuesDrawn.at(card) = values.at(cardToDraw);
+        	valuesDrawn.at(card) = values.at(cardToDraw);
             suitsDrawn.at(card) = suits.at(cardToDraw);
         }
 
