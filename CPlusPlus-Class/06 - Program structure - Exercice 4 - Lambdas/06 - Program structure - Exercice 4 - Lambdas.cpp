164e388a36e7855626be5a72abcda7d061885f9c
dorF <nemoz.burkhalter@gmail.com>
2021-10-20 15:18:38 +0200

    finished 6 exo 4

--- CPlusPlus-Class/06 - Program structure - Exercice 4 - Lambdas/06 - Program structure - Exercice 4 - Lambdas.cpp
+++ CPlusPlus-Class/06 - Program structure - Exercice 4 - Lambdas/06 - Program structure - Exercice 4 - Lambdas.cpp
@@ -11,18 +11,20 @@ int main()
     const std::vector<int> numbers = { 12,54,42,42,56,78 };
 
     // Fill the  gaps in the lambda expression, display the value of an integer
-    auto display = []() {/* Fill the code */};
+    auto display = [](int num) {std::cout << num << std::endl; };
 
     // Using the capture by reference :
+    int bigNumber = 1000;
     // Write a lambda expression substracting a parameter to a captured element into
+    auto substract = [&](int num) {bigNumber -= num; };
 
     // Use the lambda into the for loop below
     for (auto number : numbers)
     {
         // Make the substract
+        substract(number);
 
         // Display the result
+        display(bigNumber);
     }
-
-    
 }
