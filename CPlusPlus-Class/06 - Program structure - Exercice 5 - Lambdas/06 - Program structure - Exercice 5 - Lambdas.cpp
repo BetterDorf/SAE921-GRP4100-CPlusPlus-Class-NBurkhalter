4030932bb4631ccd5a3c07f2f7b0054804bc73aa
dorF <nemoz.burkhalter@gmail.com>
2021-10-20 15:41:06 +0200

    finished 6 exo 5

--- CPlusPlus-Class/06 - Program structure - Exercice 5 - Lambdas/06 - Program structure - Exercice 5 - Lambdas.cpp
+++ CPlusPlus-Class/06 - Program structure - Exercice 5 - Lambdas/06 - Program structure - Exercice 5 - Lambdas.cpp
@@ -1,7 +1,8 @@
-// 06 - Program structure - Exercice - Function pointer.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+ // 06 - Program structure - Exercice - Function pointer.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 
 #include <algorithm>
+#include <iostream>
 #include <vector>
 
 int main()
@@ -9,9 +10,10 @@ int main()
     // Some numbers
     const std::vector<int> numbers = { 12,54,42,42,56,78 };
 
-    // Use what you learned fromm previous exercice
+    int bigNumber = 1000;
+    // Use what you learned from previous exercice
 	// Use another lambda to substract and display into a for_each algorithm
     // Fill brackets with some code
-    std::for_each(numbers.begin(), numbers.end(), [] {});
+    std::for_each(numbers.begin(), numbers.end(), [&](int num){bigNumber -= num;std::cout << bigNumber << std::endl;});
 
 }
